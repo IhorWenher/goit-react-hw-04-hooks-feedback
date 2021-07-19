f95f9cd7d361fc66a0d6e356ec9b2fc7283d4d4b
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-07-19 22:58:51 +0300

    Updates

--- static/js/main.da8d98da.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(0),r=c.n(n),i=c(3),o=c.n(i),s=c(1),a=function(){return Object(s.jsx)("div",{})};c(9);o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))}},[[10,1,2]]]);
-//# sourceMappingURL=main.da8d98da.chunk.js.map
